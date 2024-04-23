19af09191e54a6a8ccd769a103bc8e90d20a3f64
Timmy Huang <46552216+timmy-huang@users.noreply.github.com>
2024-04-23 19:40:32 +1000

    Update

---
+++ sydney-dance-classes/src/components/Calendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height">
+    Schedule Classes
+  </v-container>
+</template>
+  
+<script setup lang="ts">
+  // Read data from data
+  import { ref } from 'vue'
+  import MNHurstvile from '../../data/mn-hurstville.json'
+
+  console.log(MNHurstvile)
+</script>

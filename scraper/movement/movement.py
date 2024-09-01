7688292365b5d7e64308525058f325be9abf25a0
Timmy Huang <46552216+timmy-huang@users.noreply.github.com>
2024-09-01 19:07:40 -0400

    Basic api call to access-tokens

--- scraper/movement/movement.py
+++ scraper/movement/movement.py
@@ -8,8 +8,33 @@ def movement(hauth, pauth, location, start_date, end_date):
     hlocation = location + 'movement_nation_hurstville.json'
     plocation = location + 'movement_nation_parramatta.json'
 
+    hauth = getHAuth()
+
     getData(hauth, hlocation, start_date, end_date)
     getData(pauth, plocation, start_date, end_date)
+
+def getHAuth():
+    r = requests.get('https://www.movementnation.com.au/_api/v1/access-tokens', headers={
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "cookie": "ssr-caching=cache#desc=miss#varnish=miss_miss#dc#desc=fastly_42_g",
+        "priority": "u=1, i",
+        "referer": "https://www.movementnation.com.au/",
+        "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36"
+    })
+
+    if r.status_code != 200:
+        print("Error getting auth: ", r.status_code)
+        return
+    
+    print(r.json())
+    return r.json()["apps"]
 
 def getData(auth, location, start_date, end_date):
     # Get all the data

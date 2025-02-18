854b8ef3d6cac17d4ab6e7d5846119a26ed986a3
Timmy Huang <timmyme2@gmail.com>
2025-02-19 00:30:42 +1100

    Basic mobile view

--- sydney-dance-classes/src/components/StudioIcon.vue
+++ sydney-dance-classes/src/components/StudioIcon.vue
@@ -1,32 +1,52 @@
 <template>
-  <v-img 
-    v-if="studio === 'Movement Nation Hurstville'"
-    src="/public/movement_nation_hurstville.webp"
-  />
-  <v-img 
-    v-if="studio === 'Movement Nation Parramatta'"
-    src="/public/movement_nation_parramatta.webp"
-  />
-  <v-img
-    v-if="studio === 'IMI'"
-    class="imi pa-2"
-    src="/public/imi.png"
-  />
-  </template>
+  <div class="img-container">
+    <v-img 
+      v-if="studio === 'Movement Nation Hurstville'"
+      src="/public/movement_nation_hurstville.webp"
+    />
+    <v-img 
+      v-if="studio === 'Movement Nation Parramatta'"
+      src="/public/movement_nation_parramatta.webp"
+    />
+    <v-img
+      v-if="studio === 'IMI'"
+      class="imi pa-2"
+      src="/public/imi.png"
+    />
+  </div>
+</template>
   
 <script lang="ts" setup>
   defineProps({
-    studio: String
+    studio: String,
+    mobile: {
+      type: Boolean,
+      default: false
+    }        
   })
 </script>
 
 <style>
-  .v-img {
-    width: 100px;
+  .img-container {
     height: 100px;
+    width: 100px;
+  }
+
+  .v-img {
+    width: 100%;
+    height: 100%;
+    flex-grow: 0;
   }
 
   .imi {
     background-color: black;
   }
+
+@media screen and (max-width: 600px) {
+  .img-container {
+    height: 50px;
+    width: 50px;
+  }
+  
+}
 </style>

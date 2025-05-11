ac4472b3614d9bab4cf07144b32ef379226ede41
Timmy Huang <timmyme2@gmail.com>
2025-05-11 23:08:37 +1000

    Change day to date for calendar

--- sydney-dance-classes/src/components/Calendar.vue
+++ sydney-dance-classes/src/components/Calendar.vue
@@ -16,15 +16,15 @@
     >
       <v-btn 
         variant="tonal" 
-        v-for="day in days" 
-        :key="day" 
-        :active="days.indexOf(day) === selectedDay"
-        @click="$emit('update', days.indexOf(day))"
+        v-for="date in datesForWeek" 
+        :key="date.getTime()" 
+        :active="date === selectedDate"
+        @click="$emit('update', date)"
         style="height: auto; margin-right: 8px;"
       >
         <div style="flex-direction: column;" class="pa-2">
-          <div>{{ new Date(mondayDate.getTime() + days.indexOf(day) * 24 * 60 * 60 * 1000).getDate() }}/{{ new Date(mondayDate.getTime() + days.indexOf(day) * 24 * 60 * 60 * 1000).getMonth() + 1 }}</div>
-          <div>{{ day }}</div>
+          <div>{{ date.getDate() }}/{{ date.getMonth() }}</div>
+          <div>{{ days[date.getDay()] }}</div>
         </div>
       </v-btn>
     </div>
@@ -47,16 +47,36 @@
   import { computed } from 'vue'
   
   const props = defineProps({
-    selectedDay: Number,
+    selectedDate: Date,
     mondayDate: Date
   });
+
+  let viewingCurrentWeek = true;
 
   const disablePrevious = true;
   const disableNext = true;
 
-  const days = computed(() => {
-    return ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  const datesForWeek = computed(() => {
+    // if viewing current week, return the dates for the current week
+    const arr : Date[] = [];
+    if (viewingCurrentWeek) {
+      for (let i = 0; i < 7; i++) {
+        if (props.mondayDate) {
+          arr.push(new Date(props.mondayDate.getTime() + i * 24 * 60 * 60 * 1000));
+        }
+      }
+    } else {
+      for (let i = 0; i < 7; i++) {
+        if (props.mondayDate) {
+          arr.push(new Date(props.mondayDate.getTime() + (i + 7) * 24 * 60 * 60 * 1000));
+        }
+      }
+    }
+    return arr;
   })
+
 </script>
 
 <style>
